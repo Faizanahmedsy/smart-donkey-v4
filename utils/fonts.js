780fe4e45dd95b1a36ee5196cfc85cc6876a5d4b
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-14 13:25:12 +0530

    style: added new fonts

--- utils/fonts.js
+++ utils/fonts.js
@@ -1,6 +1,17 @@
-import { Bebas_Neue, Inter, Roboto, Roboto_Condensed } from "next/font/google";
+import {
+  Bebas_Neue,
+  Inter,
+  Kulim_Park,
+  Poppins,
+  Roboto,
+  Roboto_Condensed,
+} from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+
+export const kulimPark = Kulim_Park({ subsets: ["latin"], weight: ["400"] });
+
+export const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
 
 export const bebasNeue = Bebas_Neue({ subsets: ["latin"], weight: ["400"] });
 
