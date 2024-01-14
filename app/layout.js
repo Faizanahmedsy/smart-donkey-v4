70ed38a5f80e12be3ab4660a604f9e172323d519
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-14 13:26:27 +0530

    style: minor styles changes in hero

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,10 @@ import { Bebas_Neue, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/themeProvider";
-import { roboto } from "@/utils/fonts";
+import { kulimPark, poppins, roboto } from "@/utils/fonts";
 import { Navbar } from "@/components/shared";
+
+// import image from "../public/bgpattern1.png";
 
 export const metadata = {
   title: "Smart Donkey",
@@ -14,7 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={cn("bg-background text-text min-h-screen", roboto.className)}
+        className={cn(
+          "bg-custom-image text-text min-h-screen",
+          poppins.className
+        )}
       >
         <ThemeProvider
           attribute="class"
--- components/modules/Hero.jsx
+++ components/modules/Hero.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
-import { bebasNeue, robotoCondensed } from "@/utils/fonts";
+import { poppins } from "@/utils/fonts";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col p-6 items-center min-h-screen ">
+    <div className="flex flex-col justify-center items-center min-h-[670px] text-center p-40">
       <h1
+        style={
+          {
+            // padding: "30px 300px",
+          }
+        }
         className={cn(
-          "text-[200px]  text-zinc-400   pt-24",
-          bebasNeue.className
+          "text-6xl text-zinc-400 font-[600] py-7",
+          poppins.className
         )}
       >
-        The matrix ends here
+        Become as <span className="text-blue-200">smart</span> as a fox and as
+        diligent as a <span className="text-blue-200">donkey</span>
         {/* <span className="flex justify-center items-center">
           <Image
             src="https://api.iconify.design/fluent-emoji/laptop.svg"
@@ -23,13 +29,15 @@ const Hero = () => {
         </span> */}
       </h1>
 
-      <p className={cn("text-2xl font-semibold text-zinc-500")}>
-        Learn things that actually matters in life, and make you a good and
-        successful person.
+      <p className={cn("text-2xl font-semibold text-zinc-500 px-40")}>
+        Smart Donkey is a goldmine of resources collected from all over the
+        internet to help you learn things that actually matters in life
+        {/* Learn things that actually matters in life, and makes you a good and
+        successful person. */}
       </p>
 
       <div className="flex gap-4 m-4">
-        <Button variant="outline">Learn More</Button>
+        <Button variant="outline">Explore</Button>
         <Button>Get Started</Button>
       </div>
     </div>
