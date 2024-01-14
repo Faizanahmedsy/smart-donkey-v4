1a9e3fc299180c1c4e2664b7ef3ebe2cfd4d7bf2
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-14 14:02:20 +0530

    added new font weight

--- utils/fonts.js
+++ utils/fonts.js
@@ -11,7 +11,7 @@ export const inter = Inter({ subsets: ["latin"] });
 
 export const kulimPark = Kulim_Park({ subsets: ["latin"], weight: ["400"] });
 
-export const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
+export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
 
 export const bebasNeue = Bebas_Neue({ subsets: ["latin"], weight: ["400"] });
 
