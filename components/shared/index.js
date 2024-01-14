a6e3f5cbc408f00e97f479ae02e7628aca000ef7
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-14 22:24:33 +0530

    refactor: added new imports

--- components/shared/index.js
+++ components/shared/index.js
@@ -1,5 +1,8 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import UnderConstruction from "./UnderConstruction";
+import NavButton from "./NavButton";
+import UiWrapper from "./UiWrapper";
+import ThemeSwitch from "./ThemeSwitch";
 
-export { Footer, Navbar, UnderConstruction };
+export { Footer, Navbar, UnderConstruction, NavButton, UiWrapper, ThemeSwitch };
