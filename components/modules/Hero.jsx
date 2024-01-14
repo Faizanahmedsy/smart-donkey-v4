553ad4df434c50ae157991ac7dca712506fe6180
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-14 23:17:41 +0530

    Feat: added a sexxxxyy animation to h1,p and buttons in hero

--- components/modules/Hero.jsx
+++ components/modules/Hero.jsx
@@ -11,19 +11,28 @@ const Hero = () => {
       //   background: "linear-gradient(190deg, black, #000212, #092230)",
       // }}
     >
-      <h1 className={cn("text-6xl font-bold py-7", poppins.className)}>
+      <h1
+        className={cn(
+          "animate-fade-in [--ani-delay:200ms] opacity-0 translate-y-[-1rem]  text-6xl font-bold py-7",
+          poppins.className
+        )}
+      >
         Become as <span className="text-[#99ddfd] ">smart</span> as a fox and as
         diligent as a <span className="text-[#99ddfd]">donkey</span>
       </h1>
 
-      <p className={cn("text-2xl font-semibold text-zinc-500 px-40")}>
+      <p
+        className={cn(
+          "animate-fade-in [--ani-delay:400ms] opacity-0 translate-y-[-1rem] text-2xl font-semibold text-zinc-500 px-40"
+        )}
+      >
         Smart Donkey is a goldmine of resources collected from all over the
         internet to help you learn things that actually matters in life
         {/* Learn things that actually matters in life, and makes you a good and
         successful person. */}
       </p>
 
-      <div className="flex gap-4 m-4">
+      <div className="animate-fade-in  [--ani-delay:600ms] opacity-0 translate-y-[-1rem] flex gap-4 m-4">
         <Button variant="outline">Explore</Button>
         <Button>Get Started</Button>
       </div>
