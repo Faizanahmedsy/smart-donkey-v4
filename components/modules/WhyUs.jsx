958dd9623f2e56ac5656f3ca55c5d378d04d6a5b
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-16 22:42:32 +0530

    style: made better looking why us section cards

--- components/modules/WhyUs.jsx
+++ components/modules/WhyUs.jsx
@@ -5,32 +5,32 @@ import { UiWrapper } from "../shared";
 
 const WhyUs = () => {
   return (
-    <UiWrapper>
-      <div className="flex flex-col items-center p-4 w-full gap-4 font-bold">
+    <div className="p-14">
+      <div className="flex flex-col items-center  w-full gap-4">
         <h2 className={cn("text-4xl font-bold")}>Why smart donkey?</h2>
 
         <div className="grid grid-cols-3 gap-2">
-          <Card className="flex flex-col items-center justify-center p-10">
-            <h3 className="text-2xl font-bold">We are the best</h3>
-            <p className="text-sm">
+          <Card className="flex flex-col items-center justify-center p-10 rounded-3xl ">
+            <h3 className="text-2xl font-bold pb-3">We are the best</h3>
+            <p className="text-sm text-muted-foreground">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
               voluptatum, quia, fugiat voluptate, quod quas doloribus voluptas
               quos quibusdam quae molestiae? Quisquam, quia, fugiat voluptate,
               quod quas doloribus voluptas quos quibusdam quae molestiae?
             </p>
           </Card>
-          <Card className="flex flex-col items-center justify-center p-10">
-            <h3 className="text-2xl font-bold">We are the best</h3>
-            <p className="text-sm">
+          <Card className="flex flex-col items-center justify-center p-10 rounded-3xl ">
+            <h3 className="text-2xl font-bold pb-3">We are the best</h3>
+            <p className="text-sm text-muted-foreground">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
               voluptatum, quia, fugiat voluptate, quod quas doloribus voluptas
               quos quibusdam quae molestiae? Quisquam, quia, fugiat voluptate,
               quod quas doloribus voluptas quos quibusdam quae molestiae?
             </p>
           </Card>
-          <Card className="flex flex-col items-center justify-center p-10">
-            <h3 className="text-2xl font-bold">We are the best</h3>
-            <p className="text-sm">
+          <Card className="flex flex-col items-center justify-center p-10 rounded-3xl ">
+            <h3 className="text-2xl font-bold pb-3">We are the best</h3>
+            <p className="text-sm text-muted-foreground">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
               voluptatum, quia, fugiat voluptate, quod quas doloribus voluptas
               quos quibusdam quae molestiae? Quisquam, quia, fugiat voluptate,
@@ -39,7 +39,7 @@ const WhyUs = () => {
           </Card>
         </div>
       </div>
-    </UiWrapper>
+    </div>
   );
 };
 
