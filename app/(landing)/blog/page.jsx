f1e97ac93fe0903ce4071f71b40fa2b719bdb6ca
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-24 01:03:23 +0530

    refactor: refactored the code to make it more readable

--- app/(landing)/blog/page.jsx
+++ app/(landing)/blog/page.jsx
@@ -3,7 +3,12 @@ import { BlogCard, UiWrapper, UnderConstruction } from "@/components/shared";
 import { useQuery } from "@tanstack/react-query";
 
 const Blog = () => {
-  const { status, error, data, isFetching } = useQuery({
+  const {
+    status,
+    error,
+    data: blogsData,
+    isFetching,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: async () => {
       const response = await fetch("http://localhost:3000/api/blogs");
@@ -11,18 +16,22 @@ const Blog = () => {
     },
   });
 
-  console.log({ status, error, data, isFetching });
+  console.log({ status, error, blogsData, isFetching });
 
   const showUi = true; //TODO: set to false before pushing to production
 
-  return showUi ? (
+  if (!showUi) {
+    return <UnderConstruction />;
+  }
+
+  return (
     <UiWrapper>
       <h1>Blog</h1>
       {isFetching && <p>Fetching...</p>}
       {status === "error" && <p>Error: {error.message}</p>}
       <div className="grid grid-cols-3 gap-8">
         {status === "success" &&
-          data?.blogs?.map((post) => (
+          blogsData?.blogs?.map((post) => (
             <BlogCard
               key={post.id}
               title={post.title}
@@ -31,8 +40,6 @@ const Blog = () => {
           ))}
       </div>
     </UiWrapper>
-  ) : (
-    <UnderConstruction />
   );
 };
 
