ef551557b969f0a924c1ed743f7b3abb25a21f3f
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-27 08:10:20 +0530

    feat: added blogs page redirection

--- app/(admin)/admin/components/sidebar.jsx
+++ app/(admin)/admin/components/sidebar.jsx
@@ -1,6 +1,8 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 // import { Button } from "@/registry/new-york/ui/button";
 // import { ScrollArea } from "@/registry/new-york/ui/scroll-area";
 
@@ -15,7 +17,10 @@ export function Sidebar({ className, playlists }) {
             Master
           </h2>
           <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start">
+            <Button
+              variant="secondary"
+              className="w-full justify-start hover:bg-gray-800 hover:text-white"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -31,24 +36,30 @@ export function Sidebar({ className, playlists }) {
               </svg>
               Students
             </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
+            <Link href="/admin/blogs">
+              <Button
+                variant="ghost"
+                className="w-full justify-start"
+                // onClick={() => alert("FOO")}
               >
-                <rect width="7" height="7" x="3" y="3" rx="1" />
-                <rect width="7" height="7" x="14" y="3" rx="1" />
-                <rect width="7" height="7" x="14" y="14" rx="1" />
-                <rect width="7" height="7" x="3" y="14" rx="1" />
-              </svg>
-              Courses
-            </Button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4"
+                >
+                  <rect width="7" height="7" x="3" y="3" rx="1" />
+                  <rect width="7" height="7" x="14" y="3" rx="1" />
+                  <rect width="7" height="7" x="14" y="14" rx="1" />
+                  <rect width="7" height="7" x="3" y="14" rx="1" />
+                </svg>
+                Courses
+              </Button>
+            </Link>
             <Button variant="ghost" className="w-full justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
