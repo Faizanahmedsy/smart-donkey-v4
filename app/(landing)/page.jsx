e21b473cfbc275826e4b4dad83161a850b018a63
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-28 09:46:00 +0530

    feat: integrated axios

--- app/(landing)/page.jsx
+++ app/(landing)/page.jsx
@@ -1,9 +1,5 @@
 import { BentoGrid, Hero, WhyUs } from "@/components/modules";
 import { Testimonial } from "@/components/modules/Testimonal";
-import { BackgroundBeamsDemo } from "@/components/shared/BackgroundBeams";
-import { InfiniteMovingCards } from "@/components/shared/InfiniteMovingCards";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
--- services/fetchBlogs.js
+++ services/fetchBlogs.js
@@ -1,5 +1,6 @@
+import axios from "axios";
+
 export const fetchBlogs = async () => {
-  const response = await fetch("http://localhost:3000/api/blogs");
-  const data = await response.json();
-  return data;
+  const response = await axios.get("http://localhost:3000/api/blogs");
+  return response.data;
 };
