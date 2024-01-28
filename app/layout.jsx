ccca6079a602dd51e6e646723d48185d57ed8fd5
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-28 07:51:20 +0530

    feat: added proper meta tags

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,10 +3,23 @@ import { cn } from "@/lib/utils";
 import { poppins } from "@/lib/utils/fonts";
 import Providers from "./providers";
 import { FooterSection, NavbarSection } from "@/components/shared";
+import { SITE_CONFIG } from "@/lib/constants";
 
 export const metadata = {
-  title: "Smart Donkey",
-  description: "Created by Faizan",
+  title: {
+    // en: "Faizan's Portfolio",
+    // ur: "فیضان کا پورٹ فولیو",
+    default: SITE_CONFIG.name,
+    // template: `%s |  ${SITE_CONFIG.name}`,
+    template: (title) => `${title} | ${SITE_CONFIG.name}`,
+  },
+  description: SITE_CONFIG.description,
+  icons: [
+    {
+      url: "/favicon.ico",
+      href: "/favicon.ico",
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
